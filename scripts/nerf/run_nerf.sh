578c2fbc9763d7eece30fdcf625a1ce277af903b
Michał Włodarczyk <mwlodarzc@gmail.com>
2025-10-29 15:51:15 +0100

    feat: add an example nerf running script

---
+++ scripts/nerf/run_nerf.sh
@@ -0,0 +1,6 @@
+OMP_NUM_THREADS=8 CUDA_VISIBLE_DEVICES=0 python ./main_nerf.py ../data/nerf_synthetic/drums \
+    --nn finer --lr 2e-4 --iter 37500 --downscale 4 \
+    --trainskip 4 \
+    --num_layers 4 --hidden_dim 182 --geo_feat_dim 182 --num_layers_color 4 --hidden_dim_color 182 \
+    --workspace logs/drums_finer \
+    -O --bound 1 --scale 0.8 --dt_gamma 0
