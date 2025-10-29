3ddf491c181f6d1ed6195c90ea03e1506bcfd170
Michał Włodarczyk <mwlodarzc@gmail.com>
2025-10-29 18:51:22 +0100

    fix: update path to dataset

--- scripts/nerf/run_nerf.sh
+++ scripts/nerf/run_nerf.sh
@@ -1,4 +1,4 @@
-OMP_NUM_THREADS=8 CUDA_VISIBLE_DEVICES=0 python ./main_nerf.py ../data/nerf_synthetic/drums \
+OMP_NUM_THREADS=8 CUDA_VISIBLE_DEVICES=0 python ./main_nerf.py ../data/nerf/nerf_synthetic/drums \
     --nn finer --lr 2e-4 --iter 37500 --downscale 4 \
     --trainskip 4 \
     --num_layers 4 --hidden_dim 182 --geo_feat_dim 182 --num_layers_color 4 --hidden_dim_color 182 \
